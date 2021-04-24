1cab05294b518716ae566289aed1d1b6159b5858
CaipirUltron <matheus.ferreira.reis@gmail.com>
2021-04-24 17:57:32 +0100

    Added colors to the pygame

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import pygame as pg
 pg.init()
 
 # Create the screen
-screen_resolution = (800,600)
+screen_resolution = (800,600) # has to be tuple
 screen = pg.display.set_mode(screen_resolution)
+
+# Title and Icon
+pg.display.set_caption("CaipiraGames")
+icon = pg.image.load("caipiragames.png") # doesn't work on Ubuntu (probably)
+pg.display.set_icon(icon)
 
 # Game loop
 running = True
@@ -13,3 +18,10 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+    # RGB - Red, Green, Blue
+    RGB_tuple = (255, 0, 0)
+    screen.fill(RGB_tuple)
+
+    # Updates game window
+    pg.display.update()
