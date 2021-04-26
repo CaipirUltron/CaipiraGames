04449f2131dbeb75354122de6a7267028e0b196c
CaipirUltron <matheus.ferreira.reis@gmail.com>
2021-04-26 18:36:20 +0100

    Added enemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 # Initialize pygame
 pg.init()
@@ -14,16 +15,33 @@ pg.display.set_icon(icon)
 
 # Player
 playerImg = pg.image.load("caipiragames.png")
-playerX = 0.0
-playerY = 0.0
+playerX = screen_resolution[0]/2
+playerY = screen_resolution[1]/2
 playerSpeed = 0.2
 playerSpeedX = 0.0
 playerSpeedY = 0.0
 
+# Player
+enemyImg = pg.image.load("caipiragames.png")
+enemyX = random.randint(0,screen_resolution[0]-enemyImg.get_rect().size[0])
+enemyY = random.randint(0,screen_resolution[1]-enemyImg.get_rect().size[1])
+enemySpeed = 0.2
+enemySpeedX = 0.0
+enemySpeedY = 0.0
+
 # Method for player positioning
-def move_player(x,y):
+def move_player(x, y):
+
     player_pos = (x, y)
     screen.blit(playerImg, player_pos)
+
+# Method for enemy positioning
+def move_enemy(x, y):
+
+    enemy_pos = (x, y)
+    screen.blit(enemyImg, enemy_pos)
+
+print(playerImg.get_rect().size)
 
 # Game loop
 screen_base_color_white = (255, 255, 255)
@@ -63,6 +81,18 @@ while running:
         playerX += playerSpeed*playerSpeedX/normalization_factor
         playerY += playerSpeed*playerSpeedY/normalization_factor
 
+    if playerX <= 0:
+        playerX = 0.0
+    elif playerX >= screen_resolution[0]-playerImg.get_rect().size[0]:
+        playerX = screen_resolution[0]-playerImg.get_rect().size[0]
+
+    if playerY <= 0:
+        playerY = 0.0
+    elif playerY >= screen_resolution[1]-playerImg.get_rect().size[1]:
+        playerY = screen_resolution[1]-playerImg.get_rect().size[1]
+
     # Updates game window
     move_player(playerX, playerY)
+    move_enemy(enemyX, enemyY)
+
     pg.display.update()
