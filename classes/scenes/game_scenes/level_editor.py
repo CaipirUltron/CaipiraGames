8a93de14320973aa8863a49bb684f81aabce5714
CaipirUltron <matheus.ferreira.reis@gmail.com>
2021-05-08 22:40:12 +0100

    Improved the efficiency of map generation.

--- classes/scenes/game_scenes/level_editor.py
+++ classes/scenes/game_scenes/level_editor.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from classes.scenes import Scene
 
-##############################################################################################
+
 class LevelEditor(Scene):
     """ 
     Class for polygonal tile level editor.
@@ -23,12 +23,11 @@ class LevelEditor(Scene):
         self.last_mouse_pos = np.array([self.mouse_x, self.mouse_y])
         self.curr_mouse_pos = np.array([self.mouse_x, self.mouse_y])
 
-        self.last_coords = None
-        self.curr_coords = None
-        self.paint_value = False
+        self.last_indexes = None
+        self.curr_indexes = None
+        self.value = False
 
         self.level = TileMap(16, 2000, 70, center_x=self.game.center_x, center_y=-3*self.game.center_y)
-        print(self.level.num_layers, self.level.num_sides)
 
     def eventHandler(self, event):
         if event.type == pygame.KEYDOWN:
@@ -67,35 +66,41 @@ class LevelEditor(Scene):
         '''
         Painting polygons.
         '''
-        self.last_coords = self.curr_coords
-        self.curr_coords = self.level.getGrid(self.mouse_x, self.mouse_y)
+        self.last_indexes = self.curr_indexes
+        self.curr_indexes = self.level.getIndex(self.mouse_x, self.mouse_y)
 
-        if self.curr_coords and self.clicked and self.first_selected:
-            self.paint_value = self.level.grid[self.curr_coords[1]][self.curr_coords[0]]
+        if self.curr_indexes and self.clicked and self.first_selected:
+            self.value = self.level.getValue(self.mouse_x, self.mouse_y)
             self.first_selected = False
 
-        if self.curr_coords and self.clicked and (not self.changed):
-            self.level.setGrid(*self.curr_coords, not self.paint_value)
+        if self.curr_indexes and self.clicked and (not self.changed):
+            if self.value == 0:
+                self.level.setValue(self.mouse_x, self.mouse_y, 1)
+            elif self.value == 1:
+                self.level.setValue(self.mouse_x, self.mouse_y, 0)
             self.changed = True
 
-        if (self.curr_coords != self.last_coords):
+        if (self.curr_indexes != self.last_indexes):
             self.changed = False
 
     def updateDisplay(self):
-        self.game.screen.fill(pygame.Color("Black"))
-        self.level.drawCorners(self.game.screen)
-        self.level.drawMap(self.game.screen)
+        self.level.background.fill(pygame.Color("BLACK"))
+        self.level.drawPoints()
+        self.level.drawMap()
+        self.game.screen.blit(self.level.background, (0,0))
 
-##############################################################################################
+
 class TileMap():
     '''
     Tile map functionality.
     '''
     def __init__(self, tile_size, level_radius, level_height, center_x=0.0, center_y=0.0):
         self.center_x, self.center_y = center_x, center_y
-        self.setLevel(tile_size, level_radius, level_height)
+        self.tile_color = pygame.Color("RED")
+        self.setMap(tile_size, level_radius, level_height)
+        self.computeCorners()
 
-    def setLevel(self, tile_size, level_radius, num_layers):
+    def setMap(self, tile_size, level_radius, num_layers):
         '''
         Initially configures the map according to tile size, level radius and chosen number of layers.
         '''
@@ -107,9 +112,10 @@ class TileMap():
             raise Exception("Level height must be smaller than the level radius.")
 
         self.tile_size = tile_size       # tile size, in pixels
-        self.level_radius = level_radius # level height, in pixels
-        self.num_layers = num_layers     # number of layers
+        self.level_radius = level_radius # map height, in pixels
+        self.num_layers = num_layers     # number of layers in the map
 
+        # Compute the map parameters 
         self.height_offset = self.level_radius - self.num_layers*self.tile_size
         self.num_sides = round(math.pi/math.atan2(self.tile_size, 2*self.height_offset))
         if self.num_sides < 3:
@@ -117,12 +123,14 @@ class TileMap():
         self.height_offset = self.tile_size/(2*math.tan(math.pi/self.num_sides))
         self.level_radius = self.height_offset + self.num_layers*self.tile_size
         self.angle = 2*math.pi/self.num_sides
-
-        self.grid = [[False for i in range(self.num_layers)] for j in range(self.num_sides)] # grid initialization
-
         self.level_distortion = 2*self.num_layers*math.tan(math.pi/self.num_sides)
 
-        self.computeCorners()
+        # Initialize logical grid
+        self.last_grid = np.zeros([self.num_layers, self.num_sides]) # grid initialization
+        self.curr_grid = np.zeros([self.num_layers, self.num_sides]) # grid initialization
+
+        # Create background surface
+        self.background = pygame.Surface( (math.ceil(2*self.level_radius), math.ceil(2*self.level_radius)) ).convert()
 
     def computeCorners(self):
         '''
@@ -132,8 +140,8 @@ class TileMap():
         self.num_points = self.num_sides*(self.num_layers+1)
         for angle_index in range(self.num_sides):
             for level_index in range(0, self.num_layers+1):
-                self.tiles_x.append( self.center_x + ( self.height_offset + self.tile_size*level_index )*math.cos( self.angle*angle_index - self.angle/2 ) )
-                self.tiles_y.append( self.center_y + ( self.height_offset + self.tile_size*level_index )*math.sin( self.angle*angle_index - self.angle/2 ) )
+                self.tiles_x.append( self.center_x + ( self.height_offset + self.tile_size*level_index )*math.cos( self.angle*angle_index ) )
+                self.tiles_y.append( self.center_y + ( self.height_offset + self.tile_size*level_index )*math.sin( self.angle*angle_index ) )
 
     def moveCenter(self, x, y):
         '''
@@ -144,7 +152,7 @@ class TileMap():
         self.tiles_x = (np.array(self.tiles_x)+x).tolist()
         self.tiles_y = (np.array(self.tiles_y)+y).tolist()
 
-    def getRadial(self, x, y):
+    def toPolar(self, x, y):
         '''
         Converts cartesian coords (x,y) to polar coords (radius, angle).
         '''
@@ -154,91 +162,92 @@ class TileMap():
             angle += 2*math.pi
         return radius, angle
 
-    def getIndex(self, radius, angle):
+    def setValue(self, x, y, value):
         '''
-        Returns the grid indexes at position (radius,angle), in polar coordinates. If the position is outside of the grid, returns None.
+        Sets the grid values at (x,y). If the position is outside of the grid, returns None.
         '''
-        i_index, j_index = None, None
-        for k in range(self.num_sides):
-            if angle > self.angle*k - self.angle/2 and angle < self.angle*(k+1) - self.angle/2:
-                j_index = k
-                break
-        for k in range(self.num_layers):
-            if radius > self.height_offset + self.tile_size*k  and radius < self.height_offset + self.tile_size*(k+1):
-                i_index = k
-                break
-        if i_index:
+        self.last_grid = self.curr_grid
+        indexes = self.getIndex(x, y)
+        if indexes:
+            self.curr_grid[indexes] = value
+        else:
+            return None
+
+    def getValue(self, x, y):
+        '''
+        Returns the grid value at (x,y). If the position is outside of the grid, returns None.
+        '''
+        indexes = self.getIndex(x, y)
+        if indexes:
+            return self.curr_grid[indexes]
+        else:
+            return None
+
+    def getIndex(self, x, y):
+        '''
+        Returns the grid index at position (x,y). If the position is outside of the grid, returns None.
+        '''
+        radius, angle = self.toPolar(x, y)
+        if radius > self.height_offset and radius < self.level_radius:
+            # return self.getIndex(radius, angle)
+            for k in range(self.num_sides):
+                if angle >= self.angle*k and angle < self.angle*(k+1):
+                    j_index = k
+                    break
+            for k in range(self.num_layers):
+                if radius >= self.height_offset + self.tile_size*k  and radius < self.height_offset + self.tile_size*(k+1):
+                    i_index = k
+                    break
             return i_index, j_index
         else:
             return None
 
-    def getGrid(self, x, y):
+    def drawTile(self, i, j, color):
         '''
-        Returns the grid value at position (x,y). If the position is outside of the grid, returns None.
+        Draws tile at index (i,j).
         '''
-        radius, angle = self.getRadial(x, y)
-        if radius > self.height_offset and radius < self.level_radius:
-            return self.getIndex(radius, angle)
-        else:
-            return None
+        x1 = self.center_x + ( self.height_offset + self.tile_size*i )*math.cos( self.angle*j )
+        y1 = self.center_y + ( self.height_offset + self.tile_size*i )*math.sin( self.angle*j )
 
-    def setGrid(self, i, j, *values):
+        x2 = self.center_x + ( self.height_offset + self.tile_size*(i+1) )*math.cos( self.angle*j )
+        y2 = self.center_y + ( self.height_offset + self.tile_size*(i+1) )*math.sin( self.angle*j )
+
+        x3 = self.center_x + ( self.height_offset + self.tile_size*(i+1) )*math.cos( self.angle*(j+1) )
+        y3 = self.center_y + ( self.height_offset + self.tile_size*(i+1) )*math.sin( self.angle*(j+1) )
+
+        x4 = self.center_x + ( self.height_offset + self.tile_size*i )*math.cos( self.angle*(j+1) )
+        y4 = self.center_y + ( self.height_offset + self.tile_size*i )*math.sin( self.angle*(j+1) )
+
+        pygame.draw.polygon( self.background, color, ((x1,y1),(x2,y2),(x3,y3),(x4,y4)) )
+
+    def drawMap(self):
         '''
-        Sets the grid value at (i,j) to value [True/False]
-        If no argument is passed, negates the current value.
+        Draws the map. This method can be greatly optimized by drawing the minimum amount of polygons required to represent the map, instead of simply drawing one polygon per tile.
         '''
-        if len(values) > 0:
-            for value in values:
-                self.grid[j][i] = value
-        else:
-            self.grid[j][i] = not self.grid[j][i]
-        return self.grid[j][i]
+        rows, columns = np.nonzero(self.curr_grid)
+        num_zonzero = len(rows)
+        for i in range(num_zonzero):
+            self.drawTile(rows[i], columns[i], self.tile_color)
+        print(range(num_zonzero))
 
-    def drawMap(self, screen):
-        '''
-        Draws polygons using the corner coordinates.
-        This method can be greatly optimized by drawing the minimum amount of polygons required to represent the map, instead of simply drawing one polygon per tile.
-        '''
-        for j in range(self.num_sides):
-            for i in range(self.num_layers):
-                if self.grid[j][i]:
-                    x1 = self.center_x + ( self.height_offset + self.tile_size*i )*math.cos( self.angle*j - self.angle/2 )
-                    y1 = self.center_y + ( self.height_offset + self.tile_size*i )*math.sin( self.angle*j - self.angle/2 )
-                    p1 = (x1,y1)
-
-                    x2 = self.center_x + ( self.height_offset + self.tile_size*(i+1) )*math.cos( self.angle*j - self.angle/2 )
-                    y2 = self.center_y + ( self.height_offset + self.tile_size*(i+1) )*math.sin( self.angle*j - self.angle/2 )
-                    p2 = (x2,y2)
-
-                    x3 = self.center_x + ( self.height_offset + self.tile_size*(i+1) )*math.cos( self.angle*(j+1) - self.angle/2 )
-                    y3 = self.center_y + ( self.height_offset + self.tile_size*(i+1) )*math.sin( self.angle*(j+1) - self.angle/2 )
-                    p3 = (x3,y3)
-
-                    x4 = self.center_x + ( self.height_offset + self.tile_size*i )*math.cos( self.angle*(j+1) - self.angle/2 )
-                    y4 = self.center_y + ( self.height_offset + self.tile_size*i )*math.sin( self.angle*(j+1) - self.angle/2 )
-                    p4 = (x4,y4)
-
-                    pygame.draw.polygon( screen, (255,0,0), (p1,p2,p3,p4) )
-
-    def drawCorners(self, screen):
+    def drawPoints(self):
         '''
         Draws the corner points at the screen.
         '''
-        pygame.draw.circle(screen, (255, 0, 0), ( self.center_x, self.center_y ), 1 )
+        pygame.draw.circle(self.background, self.tile_color, ( self.center_x, self.center_y ), 1 )
         for i in range(self.num_points):
-            pygame.draw.circle(screen, (255, 0, 0), ( self.tiles_x[i], self.tiles_y[i] ), 1 )
+            pygame.draw.circle(self.background, self.tile_color, ( self.tiles_x[i], self.tiles_y[i] ), 1 )
 
     def load_csv(self, filename):
-        self.grid = []
+        grid = []
         with open(os.path.join(filename)) as data:
             data = csv.reader(data, delimiter=',')
             for row in data:
-                self.grid.append(list(row))
+                grid.append(list(row))
+        self.curr_grid = np.array(grid)
 
     def create_csv(self, filename):
         with open(filename+str('.csv'), mode='w') as file:
             file_writer = csv.writer(file, delimiter=',')
-            print("Number of sides = " + str(self.num_sides))
-            for j in range(self.num_sides):
-                print(len(self.grid[j]))
-                file_writer.writerow(self.grid[j])
+            for row in range(self.num_layers):
+                file_writer.writerow(self.curr_grid[row])
