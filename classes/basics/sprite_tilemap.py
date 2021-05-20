Add BasicSprite() class, allowing proper rotation of sprites around a fixed pivot point.

classes/basics/sprite_tilemap.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os, csv, math, pygame
 from pygame.locals import *
+from classes.cameras import CameraAwareGroup
 
 
 class BasicSprite(pygame.sprite.Sprite):
@@ -9,13 +10,13 @@ class BasicSprite(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect(center=pos)
         self.angle = angle
-
+        
 
 class Tile(BasicSprite):
     '''
     Basic curved tile.
     '''
-    def __init__(self, radius, height, phi, pos, angle, color=pygame.Color("WHITE"), *groups):
+    def __init__(self, radius, height, phi, pos, angle, color=pygame.Color("RED"), *groups):
             
         self.radius = radius
         self.height = height
@@ -46,6 +47,9 @@ class Tile(BasicSprite):
             points.append( (x,y) )
         pygame.draw.polygon( self.image, self.color, points )
         
+    def update(self, mouse_r, mouse_phi):
+        pass
+
 
 class Background(BasicSprite):
     '''
@@ -60,11 +64,8 @@ class Background(BasicSprite):
         self.phi = 2*math.pi/self.num_sides
         self.color = color
 
-        self.pos = (0,0)
-        self.angle = 0
-
         image = pygame.Surface( (2*self.external_radius, 2*self.external_radius), pygame.SRCALPHA )
-        super().__init__(image, self.pos, self.angle, *groups)
+        super().__init__(image, (0,0), 0, *groups)
 
         self.drawBackground()
 
@@ -82,19 +83,17 @@ class Background(BasicSprite):
                 y = self.external_radius + ( self.internal_radius + self.tile_height*floor )*math.cos( self.phi*j - self.phi/2 )
                 pygame.draw.circle(self.image, self.color, (x,y), 1 )
 
-    def update(self, *args, **kwargs):
-        pass
 
-
-class TileMap(pygame.sprite.Group):
+class TileMap(CameraAwareGroup):
     '''
     Tile map functionality.
     '''
     default_shape = (10,100)
 
-    def __init__(self, internal_radius, tile_height, filename, color=pygame.Color("WHITE"), **kwargs):
-        super().__init__()
-        self.color = color
+    def __init__(self, target, screen_size, internal_radius, tile_height, filename, color=pygame.Color("WHITE"), **kwargs):
+        super().__init__(target, screen_size)
+        self.internal_radius = internal_radius
+        self.tile_height = tile_height
         self.filename = filename
         self.level_shape = TileMap.default_shape
 
@@ -117,7 +116,31 @@ class TileMap(pygame.sprite.Group):
             7: pygame.Color("BROWN")
         }
         self.num_materials = len(self.materials)
-        self.add( Background( internal_radius, tile_height, self.num_floors, self.num_sides ) )
+
+        self.add( Background( internal_radius, tile_height, *self.level_shape, color ) )
+        # self.load_sprites()
+
+    # def load_sprites(self):
+    #     '''
+    #     This method creates the sprites in the screen
+    #     '''
+    #     num_floors = self.level_shape[0]
+    #     num_sides = self.level_shape[1]
+
+    #     for i in range(num_floors):
+    #         for j in range(num_sides):
+    #             x = ( self.internal_radius*i )*math.cos( self.angle*angle_index )
+    #             y = self.height_offset + self.tile_size*layer_index*math.sin( self.angle*angle_index )
+                
+    #             self.add(  )
+
+    def update(self, x, y):
+        '''
+        This method calls the other update methods from the sprites, with the current mouse position in polar, world coordinates.
+        '''
+        world_pos = self.camera.from_camera( (x,y) )
+        mouse_r, mouse_phi = TileMap.to_polar(*world_pos)
+        super().update(mouse_r, mouse_phi)
 
     def load_level(self, filename):
         try:
@@ -127,8 +150,7 @@ class TileMap(pygame.sprite.Group):
                 for row in data:
                     grid.append(list(row))
             self.tilegrid = np.array(grid)
-            self.num_floors = self.tilegrid.shape[0]
-            self.num_sides = self.tilegrid.shape[1]
+            self.level_shape = self.tilegrid.shape
         except IOError:
             print("Couldn't locate "+filename+str('.csv'))
             print("Initializing grid and creating new file "+filename+str('.csv'))
@@ -138,6 +160,17 @@ class TileMap(pygame.sprite.Group):
     def save_level(self, filename):
         with open(filename+str('.csv'), mode='w', newline='') as file:
             file_writer = csv.writer(file, delimiter=',')
-            for row in range(self.num_floors):
+            for row in range(self.level_shape[0]):
                 file_writer.writerow(self.tilegrid[row].tolist())
         print("Tile map saved.")
+
+    @staticmethod
+    def to_polar(x, y):
+        '''
+        Converts cartesian coords (x,y) to polar coords (radius, angle).
+        '''
+        radius = math.sqrt( x**2 + y**2 )
+        angle = math.atan2( y, x )
+        if angle < 0:
+            angle += 2*math.pi
+        return radius, angle